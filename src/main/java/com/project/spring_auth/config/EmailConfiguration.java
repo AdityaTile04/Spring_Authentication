7f67b151d581eafd78bce9eb97061dfe427fa56f
Aditya Tile <adityatile99@gmail.com>
2025-09-15 18:51:45 +0530

    feat: add EmailConfiguration for JavaMailSender setup with Gmail SMTP

---
+++ src/main/java/com/project/spring_auth/config/EmailConfiguration.java
@@ -0,0 +1,36 @@
+package com.project.spring_auth.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfiguration {
+    @Value( "${spring.mail.username}" )
+    private String emailUsername;
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Bean
+   public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost( "smtp.gmail.com" );
+        mailSender.setPort( 587 );
+        mailSender.setUsername( emailUsername );
+        mailSender.setPassword( password );
+
+        Properties pros = mailSender.getJavaMailProperties();
+        pros.put( "mail.transport.protocol" , "smtp");
+        pros.put( "mail.smtp.auth", "true" );
+        pros.put("mail.smtp.starttls.enable", "true");
+        pros.put( "mail.debug", "true" );
+
+        return mailSender;
+
+    }
+}
