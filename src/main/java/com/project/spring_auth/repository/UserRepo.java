53a287758ee5ce0841f42955d7086d8178879ab7
Aditya Tile <adityatile99@gmail.com>
2025-09-15 14:16:05 +0530

    feat(user): add User entity and repository layer

---
+++ src/main/java/com/project/spring_auth/repository/UserRepo.java
@@ -0,0 +1,11 @@
+package com.project.spring_auth.repository;
+
+import com.project.spring_auth.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepo extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(Integer email);
+    Optional<User> findByVerificationCode(String verificationCode);
+}
